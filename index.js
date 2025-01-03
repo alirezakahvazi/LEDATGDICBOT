91fbd20ce94ffc3c32a04573775a9c987c821b0a
alirezakahvazi <104158643+alirezakahvazi@users.noreply.github.com>
2025-01-03 10:46:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createRequire } from 'module'
+import { createRequire } from 'node:module'
 const require = createRequire(import.meta.url)
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
